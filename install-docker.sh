60ad550f05d2caad7b57c6a9a481d6b1c96d35e1
Jean <jean.hamman@sharpspring.com>
2019-02-05 10:10:01 -0500

    docker isntall

---
+++ install-docker.sh
@@ -0,0 +1,6 @@
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+echo 'deb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
+apt-get update
+
+apt-get remove docker docker-engine docker.io
+apt-get install docker-ce
